ef2fdb875c8bca60d574cdb1e2f3d5c8067ac95e
Jonathan Hall <jhall22@una.edu>
2025-08-28 00:33:24 -0500

    8/27/25 Changes

--- app.mjs
+++ app.mjs
@@ -1,9 +1,37 @@
 import express from 'express'
+import path from 'path';
+// import { dirname } from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
 const app = express()
+const PORT = process.env.PORT || 3000; 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+
+// app.use(express.static(__dirname + 'public'));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello Express from Render 😍😍😍. <a href="Jonathan">Jonathan</a>')
 })
 
-app.listen(3000)
+// endpoints...middlewares...apis? 
+// send an html file
+
+app.get('/Jonathan', (req, res) => {
+  // res.send('Jonathan. <a href="/">home</a>')
+
+  res.sendFile('Jonathan.html'); 
+
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
