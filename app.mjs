43c47f8b7f73915e5ee8bb3634a6c4da535e62a2
Jonathan Hall <143550170+Jonathan-Hall34@users.noreply.github.com>
2025-08-28 00:45:38 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,36 +1,21 @@
 import express from 'express'
 import path from 'path';
-// import { dirname } from 'node:path';
-// import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from 'url';
 
 const app = express()
 const PORT = process.env.PORT || 3000; 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-// app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
-
-
-// app.use(express.static(__dirname + 'public'));
-
 
 app.get('/', (req, res) => {
   res.send('Hello Express from Render 😍😍😍. <a href="Jonathan">Jonathan</a>')
 })
 
-// endpoints...middlewares...apis? 
-// send an html file
-
 app.get('/Jonathan', (req, res) => {
-  // res.send('Jonathan. <a href="/">home</a>')
-
-  res.sendFile('Jonathan.html'); 
-
+  res.sendFile(path.join(__dirname, 'Jonathan.html')); 
 })
-
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
